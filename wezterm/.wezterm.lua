c50cc994dde9e6c0e6cf9e5aaace4e81b56cdc7a
Michael Pesa <michael.pesa@lambdal.com>
2024-11-20 11:44:57 -0700

    Update WezTerm settings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,15 +1,12 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.font = wezterm.font 'Meslo LGS NF'
-config.font_size = 10
+-- Appearance settings
+config.font = wezterm.font 'MesloLGS NF'
+config.font_size = 12
 config.color_scheme = 'Tokyo Night'
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.85
 
--- and finally, return the configuration to wezterm
 return config
