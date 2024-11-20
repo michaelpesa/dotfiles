894475ae62af803490e3c67aa2a2a42faccac63d
Michael Pesa <michael.pesa@lambdal.com>
2024-11-20 11:59:54 -0700

    Add WezTerm key bindings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,4 +10,10 @@ config.color_scheme = 'Tokyo Night'
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.85
 
+-- Use ALT+SHIFT+bracket to move tabs
+config.keys = {
+  { key = '{', mods = 'SHIFT|ALT', action = wezterm.action.MoveTabRelative(-1) },
+  { key = '}', mods = 'SHIFT|ALT', action = wezterm.action.MoveTabRelative(1) },
+}
+
 return config
