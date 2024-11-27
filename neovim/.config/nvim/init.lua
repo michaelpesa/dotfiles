d8f9110541ff0c7dd212b4037904f0c56c022098
Michael Pesa <michael.pesa@icloud.com>
2024-11-26 18:05:10 -0700

    Use tabs instead of spaces when editing Go files

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -14,3 +14,13 @@ vim.opt.shiftwidth = 2
 vim.cmd('filetype plugin indent on')
 vim.cmd('syntax enable')
 vim.cmd('colorscheme tokyonight-moon')
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.opt_local.tabstop = 8
+    vim.opt_local.softtabstop = 8
+    vim.opt_local.tabstop = 8
+    vim.opt_local.expandtab = false
+  end,
+})
