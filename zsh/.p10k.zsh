271ccbdd7a9c9b47d21f288bed74a6a47af8893e
Michael Pesa <michael.pesa@icloud.com>
2024-12-19 22:07:42 -0700

    Use custom OS icon for OpenBSD

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -190,7 +190,9 @@
   # OS identifier color.
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=
   # Custom icon.
-  # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
+  if [[ "$(uname)" == "OpenBSD" ]]; then
+    typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=''
+  fi
 
   ################################[ prompt_char: prompt symbol ]################################
   # Green prompt symbol if the last command succeeded.
